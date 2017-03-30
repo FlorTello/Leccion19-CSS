15c7052964987bde89d8e38261301687c0e072ba
Flor Tello <flor.26071994@gmail.com>
2017-03-30 17:26:37 -0500

    Agregando funcionalidad al select

--- assets/main.js
+++ assets/main.js
@@ -1,31 +1,37 @@
-var lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, accusantium laboriosam assumenda deserunt, eligendi ea quas! Incidunt saepe, commodi dolorum deserunt voluptatum, vitae minus a, sunt reiciendis perspiciatis, harum quo.";
-
-function loky(cantidad){
-	//  20 ps .
-	var form = document.getElementById("sign-up");
-	for(var i=0; i<cantidad; i++){
-		var p = document.createElement("p");
-		p.appendChild(document.createTextNode(lorem));
-		form.appendChild(p);
+var img = document.getElementsByClassName('animal');
+var select = document.getElementById('select')
+select.onchange = function(){
+	console.log(select.value);
+	if(select.value == 'orginal'){
+		for (var i = 0; i < img.length; i++) {
+				img[i].classList.add('orginal');
+				img[i].classList.remove('blanco-negro');
+				img[i].classList.remove('invertir-colores');
+				img[i].classList.remove('sepia');
+		}
 	}
-}
-
-function thor(cantidad){
-	//  20 ps .
-	var form = document.getElementById("sign-up");
-	for(var i=0; i<cantidad; i++){
-		form.innerHTML += "<p>"+lorem+"</p>";
+	if(select.value == 'sepia'){
+		for (var i = 0; i < img.length; i++) {
+				img[i].classList.remove('blanco-negro');
+				img[i].classList.remove('invertir-colores');
+				img[i].classList.remove('orginal');
+				img[i].classList.add('sepia');
+		}
 	}
-}
-
-function documnetFrag(cantidad){
-  var form = document.getElementById("sign-up");
-  var dfm = document.createDocumentFragment();
-	for(var i=0; i<cantidad; i++){
-		var p = document.createElement("p");
-		p.appendChild(document.createTextNode(lorem));
-		dfm.appendChild(p);
-  }
-  form.appendChild(dfm);
-}
-//document fragment.
+	if(select.value == 'blanco-negro'){
+		for (var i = 0; i < img.length; i++) {
+				img[i].classList.remove('orginal');
+				img[i].classList.remove('invertir-colores');
+				img[i].classList.remove('sepia');
+				img[i].classList.add('blanco-negro');
+		}
+	}
+	if(select.value == 'invertir-colores'){
+		for (var i = 0; i < img.length; i++) {
+				// img[i].classList.remove('orginal');
+				// img[i].classList.remove('blanco-negro');
+				// img[i].classList.remove('sepia');
+				img[i].classList.add('invertir-colores');
+		}
+	}
+};
